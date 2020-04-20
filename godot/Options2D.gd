243e4fc9dfacb2be2767c6ff836aced0383138b3
salvob <tholalak@yahoo.it>
2020-04-21 01:33:48 +0200

    cpu love

--- godot/Options2D.gd
+++ godot/Options2D.gd
@@ -23,8 +23,20 @@ func _on_Basic_pressed():
 	tween.interpolate_property(self, "scale", Vector2(1, 1), Vector2(1,0), 0.5,Tween.TRANS_EXPO, Tween.EASE_IN)
 	tween.start()
 	yield(tween, "tween_all_completed")
+	global.ai_love = false
 	get_tree().change_scene("res://TutorialScreen.tscn")
 
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
+
+func _on_StretchOne_pressed():
+	if tween.is_active():
+		return
+	tween.interpolate_property(self, "scale", Vector2(1, 1), Vector2(1,0), 0.5,Tween.TRANS_EXPO, Tween.EASE_IN)
+	tween.start()
+	yield(tween, "tween_all_completed")
+	global.ai_love = true
+	get_tree().change_scene("res://TutorialScreen.tscn")
+	
